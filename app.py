0defc182204a983a3c24a730474c8c8b44ed283c
Pawel <pawel98sibiga@gmail.com>
2026-04-08 18:34:02 +0200

    Add edit training function

--- app.py
+++ app.py
@@ -70,7 +70,75 @@ def add_workout():
         return redirect(url_for('mainPage'))
 
     return render_template('add_workout.html')
+@app.route('/workouts')
+def workouts_list():
+    workouts = Workout.query.order_by(Workout.id.desc()).all()
+    return render_template('workouts.html', workouts=workouts)
 
+
+@app.route('/edit_workout/<int:workout_id>', methods=['GET', 'POST'])
+def edit_workout(workout_id):
+    workout = Workout.query.get_or_404(workout_id)
+
+    if request.method == 'POST':
+        workout.name = request.form.get('workout_name')
+        workout.date = request.form.get('workout_date')
+
+        old_sets = Set.query.filter_by(workout_id=workout.id).all()
+        old_exercise_ids = [s.exercise_id for s in old_sets]
+
+        for s in old_sets:
+            db.session.delete(s)
+
+        for exercise_id in old_exercise_ids:
+            exercise = Exercise.query.get(exercise_id)
+            if exercise:
+                db.session.delete(exercise)
+
+        db.session.commit()
+
+        exercise_names = request.form.getlist('exercise_name[]')
+        sets_list = request.form.getlist('sets[]')
+        reps_list = request.form.getlist('reps[]')
+        weight_list = request.form.getlist('weight[]')
+
+        for i in range(len(exercise_names)):
+            if not exercise_names[i].strip():
+                continue
+
+            exercise = Exercise(name=exercise_names[i])
+            db.session.add(exercise)
+            db.session.commit()
+
+            new_set = Set(
+                workout_id=workout.id,
+                exercise_id=exercise.id,
+                reps=int(reps_list[i]) if reps_list[i] else 0,
+                weight=float(weight_list[i]) if weight_list[i] else 0,
+                set_number=int(sets_list[i]) if sets_list[i] else 0
+            )
+            db.session.add(new_set)
+
+        db.session.commit()
+        return redirect(url_for('workouts_list'))
+
+    workout_sets = Set.query.filter_by(workout_id=workout.id).all()
+    exercises_data = []
+
+    for s in workout_sets:
+        exercise = Exercise.query.get(s.exercise_id)
+        exercises_data.append({
+            'exercise_name': exercise.name if exercise else '',
+            'sets': s.set_number,
+            'reps': s.reps,
+            'weight': s.weight
+        })
+
+    return render_template(
+        'edit_workout.html',
+        workout=workout,
+        exercises_data=exercises_data
+    )
 
 with app.app_context():
     db.create_all()
